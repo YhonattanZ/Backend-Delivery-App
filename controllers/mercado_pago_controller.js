d278b551e40c82ffdf146764a77d6e9e9aa81632
YhonattanZ <yonattanz11@gmail.com>
2023-03-05 12:21:42 -0400

    Creando el pago

--- controllers/mercado_pago_controller.js
+++ controllers/mercado_pago_controller.js
@@ -15,7 +15,7 @@ module.exports = {
     //Datos del cliente
         const payment_data = {
             token: payment.token,
-            issuer_id: payment.issuer.id,
+            issuer_id: payment.issuer_id,
             payment_method_id: payment.payment_method_id,
             transaction_amount: payment.transaction_amount,
             installments: payment.installments,
