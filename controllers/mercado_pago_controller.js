010011f28c2a0efc27a2f4239d56e486c1d69435
YhonattanZ <yonattanz11@gmail.com>
2023-03-06 15:22:43 -0400

    Validando respuestas de mercadoPago

--- controllers/mercado_pago_controller.js
+++ controllers/mercado_pago_controller.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const mercadopago = require('mercadopago');
 const Order = require('../models/order');
 const OrderHasProducts = require('../models/order_has_products');
@@ -36,45 +37,54 @@ module.exports = {
             });
         });
         if(data){
-            console.log('DATOS DEL CLIENTE:',data.response);
-            const order = payment.order;
-            //Pago de la orden del usuario
-            Order.create(order,async (err,id)=>{
 
-                if(err){
-                    return res.status(501).json({
-                        success: false,
-                        message: 'Hubo un error al crear la orden',
-                        error: err
-                    });
-                }
-    
-                for(const product of order.products){
-                    await OrderHasProducts.create(id,product.id, product.quantity, (err,id_data)=> {
-                        if(err){
-                            return res.status(501).json({
-                                success: false,
-                                message: 'Hubo un error al crear los productos de la orden',
-                                error: err
-                            });
-                        }                    
-                    });
-                }
-    
-                return res.status(201).json({
-                    success:true,
-                    message: 'La orden ha sido pagada',
-                    data: `${id}` //ID de la categoria creada
-                });
-            });    
-             
-        }
-        else{
+    //Realizamos esta validacion para que en caso de error el servidor no caiga        
+if(data.body !== null && data.body !== undefined){
+    const order = payment.order;
+    //Pago de la orden del usuario
+    Order.create(order,async (err,id)=>{
+
+        if(err){
             return res.status(501).json({
                 success: false,
-                message: 'Error en algun dato de la peticion',
+                message: 'Hubo un error al crear la orden',
                 error: err
             });
         }
-    }
-};
+
+        for(const product of order.products){
+            await OrderHasProducts.create(id,product.id, product.quantity, (err,id_data)=> {
+                if(err){
+                    return res.status(501).json({
+                        success: false,
+                        message: 'Hubo un error al crear los productos de la orden',
+                        error: err
+                    });
+                }                    
+            });
+        }
+
+        return res.status(201).json({
+            success:true,
+            message: 'La orden ha sido pagada',
+            data: data.response
+        });        
+    });    
+}else{
+    return res.status(501).json({
+        success: false,
+        message: 'Error al crear el pago',
+        });
+} 
+
+    
+} 
+else{
+    return res.status(501).json({
+        success: false,
+        message: 'Error en algun dato de la peticion',
+        error: err
+    });
+}
+}
+}
